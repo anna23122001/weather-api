c209f45c57942a086469f2e6a029df30370a657e
anna <anna.riazantseva123321@gmail.com>
2023-07-07 12:10:11 +0300

    new version 07.07

--- src/index.js
+++ src/index.js
@@ -1,55 +1,50 @@
 'use strict'
+const btnWeather = document.querySelector('#btn-weather');
+const cityNameInp = document.querySelector('#city-name');
+const cityIdInp = document.querySelector('#city-id');
+const cityNameRadio = document.querySelector('#city-opt-name');
+const cityIdRadio = document.querySelector('#city-opt-id');
+const weatherTemp = document.querySelector('#temp');
+const weatherWindSpeed = document.querySelector('#wind-speed');
+const weatherHumidity = document.querySelector('#humidity');
 
-const cityName = document.getElementById('city-name');
-const cityId = document.getElementById('city-id');
-const getWeatherButton = document.getElementById('okButton')
-const temperatureElement = document.getElementById('temperature')
-const windSpeedElement = document.getElementById('wind- speed')
-const humidityElement = document.getElementById('humidity')
+cityIdInp.setAttribute('disabled', true);
 
-getWeatherButton.addEventListener('click', showWeather);
+cityIdRadio.addEventListener('change', () => {
+	if (cityIdRadio.checked) {
+		cityIdInp.removeAttribute('disabled');
+    cityNameInp.setAttribute('disabled', true);
+	}
+});
+cityNameRadio.addEventListener('change', () => {
+	if (cityNameRadio.checked) {
+		cityNameInp.removeAttribute('disabled');
+		cityIdInp.setAttribute('disabled', true);
+	}
+});
 
-function showWeather(){
-    let param = {};
-if(cityName.disabled){
-    const idValue = parseInt(cityId.value);
-    if(isNaN(idValue)){
-        alert('ID is not correct')
-        return;
-    }
-    param = {
-        url:'https://api.openweathermap.org/data/2.5/',
-        appid:'13a4686bc3d311361ff83aa41e62acdf',
-        cityId: idValue,
-    }
-} else{
-    const nameValue = cityName.value;
-    if(cityName.value === ''){
-        alert('Enter the name of the city');
-        return;
-    }
-    param = {
-        url:'https://api.openweathermap.org/data/2.5/',
-        appid:'13a4686bc3d311361ff83aa41e62acdf',
-        cityName: cityName.value,
-    };
+const param = {
+	url: 'https://api.openweathermap.org/data/2.5/',
+	appid: '13a4686bc3d311361ff83aa41e62acdf',
+	cityName: 'Kyiv',
+	cityId: 703448,
+};
+async function getWeather() {
+	const cityName = cityNameInp.value;
+	const cityId = cityIdInp.value;
+  const cityParam = cityNameRadio.checked ? `q=${cityName}` : `id=${cityId}`;
+	const url = `${param.url}weather?${cityParam}&units=metric&lang=uk&APPID=${param.appid}`;
+	const weatherRequest = new Request(url);
+	try {
+		const weather = await fetch(weatherRequest);
+		if (!weather.ok) throw new Error(`Error`);
+		const weatherJson = await weather.json();
+		console.log(weatherJson);
+			weatherTemp.textContent = weatherJson.main.temp + ' °C';
+			weatherWindSpeed.textContent = weatherJson.wind.speed + ' m/s';
+			weatherHumidity.textContent = weatherJson.main.humidity;
+	} catch (error) {
+		console.log(error);
+	}
 }
-
-fetch(
-        `${param.url}weather?q=${param.cityName || param.cityId}&units=metric&APPID=${param.appid}`
-    )
-.then((weather) => {
-    return weather.json();
-}).then((data) => {
-    const temperature = data.main.temp;
-    const windSpeed = data.wind.speed;
-    const humidity = data.main.humidity;
-      temperatureElement.textContent = `${temperature} K`;
-      windSpeedElement.textContent = `${windSpeed} m/s`;
-      humidityElement.textContent = `${humidity}%`;
-    })
-    .catch(error => {
-      console.log('Error:', error);
-    });
-}
-
+btnWeather.addEventListener('click', getWeather);
